c73b2ed3e58f564b86daacacbe69816a56a45971
Habib <muthaarif@gmail.com>
2022-01-18 21:16:06 +0700

    separate widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-import './question.dart';
+import './quiz.dart';
 
-import './answer.dart';
+import './result.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,23 +15,41 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  static const questions = [
+  static const _questions = [
     {
       'questionText': 'What\'s your favorit color?',
-      'answers': ['Black', 'Red', 'Green', 'White'],
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1}
+      ],
     },
     {
       'questionText': 'What\'s your favorit animal?',
-      'answers': ['Fish', 'Bird', 'Dog', 'Cat'],
+      'answers': [
+        {'text': 'Dog', 'score': 10},
+        {'text': 'Cat', 'score': 5},
+        {'text': 'Bird', 'score': 3},
+        {'text': 'Fish', 'score': 1}
+      ],
     },
     {
       'questionText': 'What\'s your favorit Dota Hero?',
-      'answers': ['CM', 'Lina', 'Dark Seer', ' Invoker'],
+      'answers': [
+        {'text': 'Invoker', 'score': 1},
+        {'text': 'CM', 'score': 1},
+        {'text': 'Lina', 'score': 1},
+        {'text': 'Dark Seer', 'score': 1}
+      ],
     }
   ];
 
   var _questionIndex = 0;
-  void _aswerQuestion() {
+  var _totalScore = 0;
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -44,21 +62,13 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text("My App"),
         ),
-        body: _questionIndex < questions.length
-            ? Column(
-                children: [
-                  Question(
-                    questions[_questionIndex]['questionText'] as String,
-                  ),
-                  ...(questions[_questionIndex]['answers'] as List<String>)
-                      .map((answer) {
-                    return Answer(_aswerQuestion, answer);
-                  })
-                ],
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questions: _questions,
+                questionIndex: _questionIndex,
               )
-            : Center(
-                child: Text('You did it'),
-              ),
+            : Result(_totalScore),
       ),
     );
   }
---
+++ lib/result.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+
+  Result(this.resultScore);
+
+  String get resultPhrase {
+    String resultText;
+    if (resultScore <= 8) {
+      resultText = 'You are awesome';
+    } else if (resultScore <= 12) {
+      resultText = 'Pretty Likeable';
+    } else if (resultScore <= 16) {
+      resultText = 'You are strange';
+    } else {
+      resultText = 'You are so bad';
+    }
+    return resultText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        resultPhrase,
+        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
