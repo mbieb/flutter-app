3719bb8544be22d3abe0b9b1c2ff26f651c1bfba
habib <muthaarif@gmail.com>
2022-01-19 10:14:09 +0700

    reset quiz

--- lib/answer.dart
+++ lib/answer.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class Answer extends StatelessWidget {
   final VoidCallback selectHandler;
   final String answerText;
+
   Answer(this.selectHandler, this.answerText);
 
   @override
